bf2e508d4f47644bd53754ef12d5b34497923459
Bryon Bean <bryonbean@gmail.com>
2021-01-31 18:27:56 -0500

    feat: adds AccountType filter

---
+++ src/Filters/AccountType.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace QBXML\Filters;
+
+use QBXML\Enums\AccountType as Type;
+
+class AccountType implements \QBXML\Queries\FilterInterface
+{
+  private Type $accountType;
+  
+  /**
+   * AccountType constructor.
+   *
+   * @param  \QBXML\Enums\AccountType  $account_type
+   */
+  public function __construct(Type $account_type)
+  {
+    $this->accountType = $account_type;
+  }
+  
+  /**
+   * @param  \QBXML\Enums\AccountType  $account_type
+   * @return $this
+   */
+  public function setAccountType(Type $account_type): self
+  {
+    $this->accountType = $account_type;
+    return $this;
+  }
+  
+  /**
+   * @inheritDoc
+   */
+  public function __toString(): string
+  {
+    return "<AccountType>{$this->accountType}</AccountType>";
+  }
+}
